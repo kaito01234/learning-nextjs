03b2731fd6b2e6342f5b71d865d6147b85370ab2
kaito01234 <43839796+kaito01234@users.noreply.github.com>
2024-05-20 06:39:14 +0000

    fix homebutton

--- app/page1/page.tsx
+++ app/page1/page.tsx
@@ -3,28 +3,18 @@
 import { useRouter } from 'next/navigation'
 
 export default function Page() {
-  const healthcheck = () => {
-    const request = new Request(`${process.env.NEXT_PUBLIC_TEST_URL}/migrationtest/api/health`);
-    fetch(request).then((response) => console.log(response))
-  }
-
   const router = useRouter()
   return (
     <div>
-      <button type="button" onClick={() => router.push('/home')}>
-        Home
-      </button>
-      <button type="button" onClick={() => router.push('/migrationtest')}>
+      <a href={`${process.env.NEXT_PUBLIC_TEST_URL}/home`}>Home</a>
+      <button type="button" onClick={() => router.push('/')}>
         Migrationtest
       </button>
-      <button type="button" onClick={() => router.push('/migrationtest/page2')}>
+      <button type="button" onClick={() => router.push('/page2')}>
         page2
       </button>
-      <button type="button" onClick={() => fetch(`/migrationtest/api/health/`, {})}>
+      <button type="button" onClick={() => fetch(`/migrationtest/api/health`, {})}>
         fetch
-      </button>
-      <button type="button" onClick={healthcheck}>
-        healthcheck
       </button>
     </div>
   )
